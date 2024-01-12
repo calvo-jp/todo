a7cd00ff04ab1ce1cc5d570a98b631bf1cc30668
jp calvo <calvojp92@gmail.com>
2024-01-12 17:11:43 +0800

    init layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,32 @@
 <script lang="ts">
 	import '../app.css';
 
-	const {children} = $props();
+	const {children, data} = $props();
 </script>
 
-{@render children()}
+<header class="flex items-center px-4 py-2">
+	<div>Logo</div>
+
+	<div class="grow" />
+
+	{#if data.session}
+		<div>Hello, {data.session.user?.name}</div>
+	{:else}
+		<nav>
+			<ul class="flex gap-2">
+				<li>
+					<a href="/login">Login</a>
+				</li>
+				<li>
+					<a href="/register">Register</a>
+				</li>
+			</ul>
+		</nav>
+	{/if}
+</header>
+
+<main>
+	{@render children()}
+</main>
+
+<footer></footer>
