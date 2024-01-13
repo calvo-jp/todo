45cd31eaec498d9cf2270a2958618da9b42d3b5a
jp calvo <calvojp92@gmail.com>
2024-01-13 18:54:31 +0800

    add logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import {enhance} from '$app/forms';
 	import '../app.css';
+	import Logo from './logo.svelte';
 
 	const {children, data} = $props();
 </script>
 
 {#if data.user}
 	<header class="flex gap-2 border-b border-gray-200 px-8 py-5">
-		<div>Logo</div>
+		<div>
+			<Logo />
+		</div>
 		<div class="grow" />
 		<div class="flex items-center gap-2">
 			<div class="leading-none text-gray-700">
